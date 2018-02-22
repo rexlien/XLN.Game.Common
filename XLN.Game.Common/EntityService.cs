c8f965829f6bfd3e9df594055a1751fea7e0d2d0
rex.lien <line@TW-LW10219-M-2.local>
2018-02-22 15:27:40 +0800

    Added test thrift integration

---
+++ XLN.Game.Common/EntityService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime;
+using Thrift.Protocol;
+using Thrift.Transport;
+
+namespace XLN.Game.Common
+{
+    public class EntityService
+    {
+
+        public EntityService()
+        {
+            SendEntityMessage();
+        }
+
+        public void SendEntityMessage()
+        {
+            TTransport transport = new TSocket("localhost", 10000);
+            TProtocol protocol = new TCompactProtocol(transport);
+            Thrift.EntityService.Client client = new Thrift.EntityService.Client(protocol);
+        }
+    }
+}
